e51b2e4131ad639630b1e46203ddbbae2b86a143
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-05 13:44:39 +0530

    Add files via upload

---
+++ src/test/java/Project1/Arithematic.java
@@ -0,0 +1,18 @@
+
+    class Arithematic
+    {
+        public static void main(String[] args)
+        {
+
+
+            int a = 7, b = 9;
+
+
+            System.out.println("a + b = " + (a + b));
+            System.out.println("a - b = " + (a - b));
+            System.out.println("a * b = " + (a * b));
+            System.out.println("a / b = " + (a / b));
+            System.out.println("a % b = " + (a % b));
+        }
+    }
+
