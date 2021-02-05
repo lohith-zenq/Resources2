44de3b58e9f370efd8452e1c15e98f37f3d598da
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-05 15:09:07 +0530

    Update Arithematic.java

--- src/test/java/Project1/Arithematic.java
+++ src/test/java/Project1/Arithematic.java
@@ -2,34 +2,31 @@ package Task4;
 
 public class Arithmeticop
 {
-	static int a=8,b=5;
+	static int a=8,b=5,c;
 	
 	public static void add()
 	{
-		int c;
 		c=a+b;
 		System.out.println(c);
 	}
 	public static void sub()
 	{
-		int c;
 		c=a-b;
 		System.out.println(c);
 	}
 	public static void mul()
 	{
-		int c;
 		c=a*b;
 		System.out.println(c);
 	}
 	public static void div()
 	{
-		int c;
 		c=a/b;
 		System.out.println(c);
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+       {
        add();
        sub();
        mul();
