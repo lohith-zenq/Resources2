8a18c2b9f89ceead86f26ddc3ce335c39493a9c7
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-08 18:02:50 +0530

    Add files via upload

--- src/test/java/Project1/Calculator.java
+++ src/test/java/Project1/Calculator.java
@@ -1,14 +1,20 @@
 package Selbasics;
+
+ import org.testng.Assert;
+ import org.testng.annotations.Test;
+
 public class Calculator
 {
 
-
+@Test
     public static void main(String args[])
     {
+
         int result;
         int operand1, operand2;
         Operations operations = new Operations();
         result = operations.division(10, 5);  // 5-divisor,10-dividend
+        Assert.assertEqual(result = operations.division(10, 5),2);
         System.out.println("result is " + result);
     }
 }
--- src/test/java/Project1/Operations.java
+++ src/test/java/Project1/Operations.java
@@ -1,20 +1,26 @@
 package Selbasics;
 
-public class Operations {
+public class Operations
+{
+
     public int addition(int operand1, int operand2)
     {
         return operand1 + operand2;
     }
 
-    public int subtraction(int operand1, int operand2) {
+
+    public int subtraction(int operand1, int operand2)
+    {
         return operand1 - operand1;
     }
 
-    public int multiplication(int operand1, int operand2) {
+    public int multiplication(int operand1, int operand2)
+    {
         return operand1 * operand2;
     }
 
-    public int division(int operand1, int operand2) {
+    public int division(int operand1, int operand2)
+    {
         return operand1 / operand2;
     }
 }
