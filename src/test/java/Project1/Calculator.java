82d0149f2c3605b93a6c4bca894c0a766cd8995d
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-09 10:34:38 +0530

    Update Calculator.java

--- src/test/java/Project1/Calculator.java
+++ src/test/java/Project1/Calculator.java
@@ -14,7 +14,8 @@ public class Calculator
         int operand1, operand2;
         Operations operations = new Operations();
         result = operations.division(10, 5);  // 5-divisor,10-dividend
-        Assert.assertEqual(result = operations.division(10, 5),2);
+     
+        Assert.assertEqual(result = operations.division(10, 5),2);      //Assertion statement 
         System.out.println("result is " + result);
     }
 }
