869bfca93d406531dcfa3eb48678e8f4ae7783bc
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-09 16:08:49 +0530

    Code with inheritance and method overloading
    Code with inheritance and method overloading

---
+++ src/test/java/Project1/Gmail.java
@@ -0,0 +1,13 @@
+package Selbasics;
+
+public class Gmail
+{
+    public static void main(String args[])
+    {
+        ScenariosforGmail s1=new ScenariosforGmail();
+        s1.testscenarios();
+        ScenariosforGmail.login l1=new ScenariosforGmail.login();
+        l1.testscenarios();
+    }
+
+}
