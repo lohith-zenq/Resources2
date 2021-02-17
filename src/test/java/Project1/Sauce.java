b1c9f09411ec201beca2bc82c3ef8d91304acab8
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-17 09:39:47 +0530

    Swaglabs login (chrome,firefox)

--- src/test/java/Project1/Sauce.java
+++ src/test/java/Project1/Sauce.java
@@ -17,12 +17,15 @@ public class Sauce {
         {
             System.setProperty("webdriver.chrome.driver", "resources1//chromedriver.exe");
             WebDriver driver = new ChromeDriver();
+           
             driver.get("https://www.saucedemo.com/index.html");
             driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("lohith-zenq");
             driver.findElement(By.cssSelector("input[id='password']")).sendKeys("123321123");
+            
             Thread.sleep(2000);     //time gap before clicking "login" button
             driver.findElement(By.id("login-button")).click();
             System.out.println(driver.getTitle());
+           
             Assert.assertEquals(driver.getTitle(),"Swag Labs");
             driver.quit();
         }
@@ -32,13 +35,16 @@ public class Sauce {
             System.setProperty("webdriver.gecko.driver", "resources1/geckodriver.exe");
             WebDriver driver = new FirefoxDriver();
             driver.get("https://www.saucedemo.com/index.html");
+           
             driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("lohith-zenq");
             driver.findElement(By.cssSelector("input[id='password']")).sendKeys("123321123");
+            
             Thread.sleep(2000);
             driver.findElement(By.id("login-button")).click();
             driver.getTitle();
             System.out.println(driver.getTitle());
-            Assert.assertEquals(driver.getTitle(),"Swag Labs");
+           
+            SAssert.assertEquals(driver.getTitle(),"Swag Labs");
             driver.quit();
 
         }
