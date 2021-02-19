a4e6db4abf69e216875ae7be9b95cd19df48f5fa
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-19 14:36:29 +0530

    Code with test annotations and priority

---
+++ src/test/java/Project1/testNg/Testannotation2.java
@@ -0,0 +1,27 @@
+package testNG;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Testannotation2
+{
+    @BeforeMethod
+    public void precondition()
+    {
+        System.out.println("before login check whether we are having any precondition");
+    }
+    @AfterMethod
+    public void testdata()
+    {
+        System.out.println("Testdata-- username=rahul@gmail.com password=123321123");
+    }
+    @Test
+    public void logindetails()
+    {
+        System.out.println("Enter username from given testdata");
+        System.out.println("Enter password from given testdata");
+    }
+
+
+}
