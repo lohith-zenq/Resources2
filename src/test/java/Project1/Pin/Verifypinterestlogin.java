04ff5ae138dc5209b9b761300d2d6b8faebd6640
lohith-zenq <78469366+lohith-zenq@users.noreply.github.com>
2021-02-23 13:23:05 +0530

    Testcases for Pinterest_site_login

---
+++ src/test/java/Project1/Pin/Verifypinterestlogin.java
@@ -0,0 +1,44 @@
+package Pin.testcases;
+
+import com.pinterest.login.Loginpage;
+import com.pinterest.login.Logout;
+import com.pinterest.login.Saveimage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Verifypinterestlogin
+{
+    @Test
+    public void valid_login() throws InterruptedException
+    {
+        System.setProperty("webdriver.chrome.driver", "resources1//chromedriver.exe");
+       WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://in.pinterest.com/login/");
+
+       Loginpage login=new Loginpage(driver);
+
+        login.user_name("kanigallalohith@gmail.com");
+        login.user_password("lohith123@");
+        login.login_button();
+      Thread.sleep(Long.parseLong("2000"));
+
+        Logout out=new Logout(driver);
+
+        out.options();
+        out.logingout();
+
+        //Saveimage save=new Saveimage(driver);
+
+        //save.image_selection();
+        //save.save_image();
+
+
+    }
+
+
+
+
+
+}
